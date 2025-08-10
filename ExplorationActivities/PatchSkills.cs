0a893c872c520a983a0dcbfc4af4c6f403f8df3e
SilchasRuin <jcbcody@outlook.com>
2025-08-10 06:04:12 -0400

    updated harmony

--- ExplorationActivities/PatchSkills.cs
+++ ExplorationActivities/PatchSkills.cs
@@ -9,12 +9,9 @@ internal static class PatchSkillDescription
 {
     internal static bool Prefix(Skill skill, ref string __result)
     {
-        if (skill == ModData.Skills.WarfareLore)
-        {
-            __result = "You have studied battlefields, tactics, and strategy.\n\nThis skill is only used in conjunction with the {b}Battle Planner{/b} skill feat and {i}DawnniEx's{/i} {b}Recall Weakness{/b} action.";
-            return false;
-        }
-        return true;
+        if (skill != ModData.Skills.WarfareLore) return true;
+        __result = "You have studied battlefields, tactics, and strategy.\n\nThis skill is only used in conjunction with the {b}Battle Planner{/b} skill feat and {i}DawnniEx's{/i} {b}Recall Weakness{/b} action.";
+        return false;
     }
 }
 [HarmonyPatch(typeof(Skills), nameof(Skills.SkillToTrait))]
@@ -22,12 +19,9 @@ internal static class PatchSkillToTrait
 {
     internal static bool Prefix(Skill skill, ref Trait __result)
     {
-        if (skill == ModData.Skills.WarfareLore)
-        {
-            __result = ModData.Traits.WarfareLore;
-            return false;
-        }
-        return true;
+        if (skill != ModData.Skills.WarfareLore) return true;
+        __result = ModData.Traits.WarfareLore;
+        return false;
     }
 }
 [HarmonyPatch(typeof(Skills), nameof(Skills.TraitToSkill))]
@@ -35,24 +29,37 @@ internal static class PatchTraitToSkill
 {
     internal static bool Prefix(Trait skill, ref Skill? __result)
     {
-        if (skill == ModData.Traits.WarfareLore)
-        {
-            __result = new Skill?(ModData.Skills.WarfareLore);
-            return false;
-        }
-        return true;
+        if (skill != ModData.Traits.WarfareLore) return true;
+        __result = new Skill?(ModData.Skills.WarfareLore);
+        return false;
     }
 }
-/*[HarmonyPatch(typeof(Skills), nameof(Skills.SkillToFeat))]
+[HarmonyPatch(typeof(Skills))]
+[HarmonyPatch(nameof(Skills.SkillToFeat), typeof(Skill))]
 internal static class PatchSkillToFeat
 {
     internal static bool Prefix(Skill skill, ref FeatName __result)
     {
-        if (skill == ModData.Skills.WarfareLore)
-        {
-            __result = ModData.FeatNames.WarfareLore;
-            return false;
-        }
-        return true;
+        if (skill != ModData.Skills.WarfareLore) return true;
+        __result = ModData.FeatNames.WarfareLore;
+        return false;
     }
-}*/
+}
+[HarmonyPatch(typeof(Skills))]
+[HarmonyPatch(nameof(Skills.SkillToFeat), typeof(Skill), typeof(Proficiency))]
+internal static class PatchSkillToFeat2
+{
+    internal static bool Prefix(Skill skill, Proficiency proficiency, ref FeatName __result)
+    {
+        if (skill != ModData.Skills.WarfareLore) return true;
+        __result = proficiency switch
+        {
+            Proficiency.Trained => ModData.FeatNames.WarfareLore,
+            Proficiency.Expert => ModData.FeatNames.WarfareLoreExpert,
+            Proficiency.Master => ModData.FeatNames.WarfareLoreMaster,
+            Proficiency.Legendary => ModData.FeatNames.WarfareLoreLegendary,
+            _ => throw new ArgumentOutOfRangeException(nameof(proficiency), proficiency, null)
+        };
+        return false;
+    }
+}
